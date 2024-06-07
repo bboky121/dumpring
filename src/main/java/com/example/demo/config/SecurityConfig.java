8cebdf22fb937a1c91e974f28957946e3b767f78
bboky121 <bboky121@gmail.com>
2024-06-07 15:18:56 +0900

    init google OAuth2 적용

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo.config;
 
 import com.example.demo.security.JwtTokenFilter;
+import com.example.demo.service.CustomOAuth2UserService;
 import com.example.demo.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +15,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -26,6 +28,9 @@ public class SecurityConfig {
 
     @Autowired
     private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    private CustomOAuth2UserService customOAuth2UserService;
 
     @Autowired
     private JwtTokenFilter jwtTokenFilter;
@@ -51,17 +56,24 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/users").permitAll() // 사용자 생성은 모든 사용자에게 허용
-                        .requestMatchers("/admin/**").hasRole("ADMIN") // 인증된 사용자만 접근 가능
-                        .requestMatchers("/users/**").hasAnyRole("USER", "ADMIN") // 인증된 사용자만 접근 가능
-                        .anyRequest().permitAll() // 그 외 요청은 모두 접근 가능
-                )
-                .formLogin(form -> form
-                        .loginPage("/login").defaultSuccessUrl("/users", true).permitAll() // 사용자 정의 로그인 페이지 설정
-                )
-                .logout(LogoutConfigurer::permitAll); // 로그아웃 설정
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/oauth2/**", "/login/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/users").permitAll() // 사용자 생성은 모든 사용자에게 허용
+                .requestMatchers("/users/**").hasAnyRole("USER", "ADMIN") // 인증된 사용자만 접근 가능
+                .requestMatchers("/admin/**").hasRole("ADMIN") // 인증된 사용자만 접근 가능
+                .anyRequest().authenticated() // 그 외 요청은 모두 인증된 사용자만 접근 가능
+            )
+            .oauth2Login(withDefaults -> {
+                withDefaults.loginPage("/login");
+                withDefaults.defaultSuccessUrl("/users", true);
+                withDefaults.userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService));
+            })
+            .formLogin(form -> form
+                    .loginPage("/login").defaultSuccessUrl("/users", true).permitAll() // 사용자 정의 로그인 페이지 설정
+            )
+            .logout(LogoutConfigurer::permitAll); // 로그아웃 설정
 
         http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
---
+++ src/main/java/com/example/demo/service/CustomOAuth2UserService.java
@@ -0,0 +1,38 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        System.out.println("userRequest = " + userRequest);
+        System.out.println("oauth2user.getA~~~");
+        System.out.println(oAuth2User.getAuthorities());
+        String email = oAuth2User.getAttribute("email");
+        String name = oAuth2User.getAttribute("name");
+
+        Optional<User> userOptional = userRepository.findByEmail(email);
+
+        if (!userOptional.isPresent()) {
+            User user = new User();
+            user.setEmail(email);
+            user.setName(name);
+            user.setRole("USER");
+            userRepository.save(user);
+        }
+        return oAuth2User;
+    }
+}
